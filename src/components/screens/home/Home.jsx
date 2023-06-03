616b838678df6ee241f7e6d69057c8fc6877fe9b
Aleksandr <shunko1999@gmail.com>
2023-06-03 16:30:47 +0300

    feat: Add filter by price and hook useMemo for caching

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useMemo } from "react";
 import styles from './Home.module.css'
 import {cars} from './cars.data.js'
 import CarItem from "./car-item/CarItem";
 
 function Home() {
+const filteredCars = useMemo(() => cars.filter(car => car.price > 200000), [])
+
+
   return (
 <div> 
     <h1>Cars catalog</h1>
     <div>
-      {cars.length ? (
-        cars.map(car => <CarItem key= {car.id} car=
+      {filteredCars.length ? (
+        filteredCars.map(car => <CarItem key= {car.id} car=
           {car}/>)
       ) : (
       <p>There are no cars</p>
--- src/components/screens/home/cars.data.js
+++ src/components/screens/home/cars.data.js
@@ -8,7 +8,25 @@ image: '../public/bmw_one.jpeg',
     {
         id: 2,
         name: 'BMW',
-        price: 30970,
+        price: 990970,
         image: '../public/bmw_one.jpeg',
-    }
+    },
+    {
+        id: 3,
+        name: 'Toy',
+        price: 40970,
+        image: '../public/bmw_one.jpeg',
+    },
+    {
+        id: 4,
+        name: 'Huav',
+        price: 60970,
+        image: '../public/bmw_one.jpeg',
+    },
+    {
+        id: 5,
+        name: 'Tesla',
+        price: 90970,
+        image: '../public/bmw_one.jpeg',
+    },
 ]
