2e6206647f92f4234384a332cbd1a496b258b500
Aleksandr <shunko1999@gmail.com>
2023-06-04 13:45:03 +0300

    feat: optimization code

--- src/components/screens/home/create-car-form/CreateCarForm.jsx
+++ src/components/screens/home/create-car-form/CreateCarForm.jsx
@@ -1,29 +1,49 @@
 import { useState } from 'react'
 import styles from './CreateCarForm.module.css'
 
-const CreateCarForm = ({setCars}) => {
-    const [name, setName] = useState('')
-    const [price, setPrice] = useState('')
-    const [image, setImage] = useState('')
+const clearData = {
+    price: '', 
+    name: '',
+    image: '',
+}
 
+const CreateCarForm = ({setCars}) => {
+    const [data, setData] = useState(clearData)
+    
     const createCar = (e) => {
         e.preventDefault()
 
-       setCars(prev => [...prev, {id: prev.length, name, price, image}])
+       setCars(prev => [
+        {
+        id: prev.length + 1, 
+        ...data
+    }, 
+    ...prev,
+    ])
+
+    setData(clearData)
     }
-    
+
     return <form className={styles.form}>
         <input 
         placeholder="Name" 
-        onChange ={e => setName(e.target.value)} 
-        value={name} 
+        onChange ={e => setData(prev => ({
+            ...prev, name: e.target.value
+        }))} 
+        value={data.name} 
         />
         <input placeholder="Price" 
-         onChange ={e => setPrice(e.target.value)} 
-         value={price} />
+        onChange ={e => setData(prev => ({
+            ...prev, price: e.target.value
+        }))} 
+        value={data.price} 
+        />
         <input placeholder="Image"
-         onChange ={e => setImage(e.target.value)} 
-         value={image}  />
+         onChange ={e => setData(prev => ({
+            ...prev, image: e.target.value
+        }))} 
+        value={data.image} 
+        />
 
         <button className='btn' onClick={e => createCar(e)}>Create</button>
     </form>
