661f4eadec860f1ca7bb219fbc9edf9db0d508a2
Aleksandr <shunko1999@gmail.com>
2023-06-04 13:56:47 +0300

    feat: use hook useEffect with async and await

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import styles from './Home.module.css'
 import {cars as carsData} from './cars.data.js'
 import CarItem from "./car-item/CarItem";
@@ -6,7 +6,30 @@ import CreateCarForm from "./create-car-form/CreateCarForm";
 
 function Home() {
 const [cars, setCars] = useState(carsData)
-console.log(cars)
+
+useEffect(() => {
+  console.log('hey')
+}, [cars]) //hook useEffect Что-то выполняет при каких-то зависимостях,если cars изменится, то выведится hey
+//Получить данные с сервера для useEffect
+/*
+useEffect(() => {
+  console.log('hey')
+}, [cars])
+
+useEffect(() => {
+  const fetchData = async () => {
+    const response =await fetch('http://localhost:4200/cars')
+  const data = await response.json()
+  setCars(data)
+}
+setCars(data)
+}
+fetchData()
+}, [])
+*/
+
+
+
   return (
 <div> 
     <h1>Cars catalog</h1>
